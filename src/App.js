f0148c7b3ff6914d559f15e0b9dd2c56d25febc5
ARJUN2416 <arjunrocks152@gmail.com>
2023-08-21 10:50:47 +0530

    prop changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import './App.css';
 import Navbar from './component/Navbar';
 import TextForm from './component/TextForm';
-import About from './component/About';
+
 import Alert from './component/Alert';
 import { useState } from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
+
 
 function App() {
   const [mode, setMode] = useState("light")
@@ -54,23 +50,17 @@ showAlert("Dark mode disabled", "success")
   return  (
     <>
      
-      <Alert alert={alert} />
+      
      
-      <Router>
+      
       <Navbar title="TextUtil" mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert} />
+
       <div className="container">
-         <Switch>
-          <Route path="/about">
-            <About/>
-          </Route>
-        
-          <Route path="/home">
-          <TextForm heading="Enter the text to analyze" mode={mode} />
-          </Route>
-        </Switch>
-        </div>
-        </Router>
-  
+       <TextForm heading="Enter the text to analyze" mode={mode} />
+       
+       
+       </div>
     </>
   );
 }
